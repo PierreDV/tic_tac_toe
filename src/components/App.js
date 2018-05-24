d8a9d03fba4a2f7eb98701c7b0596de72a75e999
PierreDV <p.verteuil@gmail.com>
2018-05-24 17:49:17 +0200

    added method to check if a player has won

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import _ from 'underscore';
 import Header from './Header';
 import Board from './Board';
+
+const WINNING_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      result: null,
       turn: "O",
       squares: [
         null,
@@ -22,6 +35,28 @@ class App extends Component {
     };
 
     this.placePiece = this.placePiece.bind(this);
+  }
+
+  checkResult() {
+    if (this.state.turn === "O") {
+      this.collectIndexes(this.state.squares, "X");
+    } else {
+      this.collectIndexes(this.state.squares, "O");
+    }
+  }
+
+  collectIndexes(arr, val) {
+    let indexes = [], i;
+    for(i = 0; i < arr.length; i++) {
+      if (arr[i] === val) {
+        indexes.push(i);
+      }
+    }
+    WINNING_COMBINATIONS.forEach((combo) => {
+      if(_.intersection(combo, indexes).length === combo.length) {
+        console.log(`${val} has won!`);
+      }
+    });
   }
 
   placePiece(event, turn) {
@@ -52,6 +87,7 @@ class App extends Component {
   }
 
   render() {
+    this.checkResult();
     return (
       <div className="App">
         <Header turn={this.state.turn} />
