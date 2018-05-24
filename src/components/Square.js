c0ea6acf0960ce42e68c2bd6996cb250277eacbd
PierreDV <p.verteuil@gmail.com>
2018-05-24 13:46:20 +0200

    added click event to squares to add x when clicked

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Square = (props) => {
   return (
-    <div className="Square">X</div>
+    <div onClick={(e) => {e.target.innerHTML = 'X'}} className="Square"></div>
   )
 }
 
