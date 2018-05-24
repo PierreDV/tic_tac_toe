bf22b97c7271d8a842cb633f365b2cef764a8a36
PierreDV <p.verteuil@gmail.com>
2018-05-24 12:18:30 +0200

    added css and flexbox layout to establish grid

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,21 +4,19 @@ import Square from './Square';
 const Board = () => {
   return (
     <div className="Board">
-      <div className="row">
+  
         <Square />
         <Square />
         <Square />
-      </div>
-      <div className="row">
+  
         <Square />
         <Square />
         <Square />
-      </div>
-      <div className="row">
+
         <Square />
         <Square />
         <Square />
-      </div>
+ 
     </div>
   );
 }
